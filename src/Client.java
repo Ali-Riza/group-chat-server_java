07d8ec5b835699c5c65077fead0cb644229b4f65
Smen04 <s.mengillioglu@hotmail.com>
2023-01-18 18:12:43 +0100

    Client

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,25 @@
+import java.util.ArrayList;
 
 public class Client {
-
+	
+	private ArrayList<Client> clients = new ArrayList<Client>();
+	private int id;
+	private int port;
+	private ArrayList<String> nachrichten = new ArrayList<String>();
+	
+	public Client(int port) {
+		id = clients.size() + 1;
+		clients.add(this);
+		this.port = port;
+	}
+	
+	public void sendeNachricht(String nachricht) {
+		Socket clientSocket = new Socket("localhost",port);
+		clientSocket.write(nachricht);
+		
+		if(nachricht.contains(":")) {
+			String[] arr = nachricht.split(":");
+		}
+	}
+	
 }
