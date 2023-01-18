01a93b90cceb1ed3d69bab1b19a896689f21b452
Smen04 <s.mengillioglu@hotmail.com>
2023-01-18 18:39:55 +0100

    Client

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,8 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Client {
 	
@@ -6,20 +10,44 @@ public class Client {
 	private int id;
 	private int port;
 	private ArrayList<String> nachrichten = new ArrayList<String>();
+	private String userName;
 	
-	public Client(int port) {
+	public Client(int port, String userName) {
 		id = clients.size() + 1;
 		clients.add(this);
 		this.port = port;
+		this.userName = userName;
 	}
 	
-	public void sendeNachricht(String nachricht) {
+	public void sendeNachricht(String nachricht) throws UnknownHostException, IOException {
 		Socket clientSocket = new Socket("localhost",port);
-		clientSocket.write(nachricht);
-		
-		if(nachricht.contains(":")) {
-			String[] arr = nachricht.split(":");
+		Scanner scanner = new Scanner(System.in);
+		nachricht = scanner.nextLine();
+		//clientSocket.write(userName + ": " + nachricht);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public ArrayList<String> getNachrichten() {
+		return nachrichten;
+	}
+	
+	public void getNachricht(int id) {
+		for(int i = 0; i < clients.size(); i++) {
+			if(clients.get(i).getId() == id) {
+				clients.get(i).getNachrichten();
+			}
 		}
 	}
 	
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter your username for the chat groupe ");
+		String username = scanner.nextLine();
+		Client client = new Client(8002, username);
+		client.getNachricht(client.getId());
+		client.sendeNachricht("Hallo");
+	}
 }
