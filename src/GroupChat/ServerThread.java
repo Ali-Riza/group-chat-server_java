044c6c4ec4e6afd0d1517b4f009fd8ace0483f33
Smen04 <s.mengillioglu@hotmail.com>
2023-01-19 19:18:22 +0100

    Update ServerThread.java

--- src/GroupChat/ServerThread.java
+++ src/GroupChat/ServerThread.java
@@ -6,27 +6,16 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class ServerThread extends Thread {
-<<<<<<< HEAD
-	public static ArrayList<ServerThread> ServerThreads = new ArrayList<>();
-=======
 	
 	public static ArrayList<ServerThread> serverThreads = new ArrayList<ServerThread>();
 	private BufferedReader bufferedReader;
 	private BufferedWriter bufferedWriter;
->>>>>>> 07b9a8c93f725127e2f4d2dcdd997757fe58bc34
 	private String clientUserName;
-	private Socket cs;
-	private Server s;
+	private Socket clientSocket;
+	private Server server;
 
-	public ServerThread(Socket cs, Server s) {
+	public ServerThread(Socket socket, Server server) {
 		try {
-<<<<<<< HEAD
-			this.s = s;
-			this.cs = cs;
-			this.clientUserName = cs.getInBR().readLine();
-			ServerThreads.add(this);
-			broadcastMessage("SERVER: " + clientUserName + " has entred the chat!");
-=======
 			this.server = server;
 			this.clientSocket = socket;
 			this.bufferedWriter = clientSocket.getOutBR();
@@ -34,44 +23,29 @@ public class ServerThread extends Thread {
 			this.clientUserName = bufferedReader.readLine();
 			serverThreads.add(this);
 			nachrichtAnJedenVersenden("SERVER: " + clientUserName + " hat den Gruppenchat betreten!");
->>>>>>> 07b9a8c93f725127e2f4d2dcdd997757fe58bc34
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 	public void run() {
-		String messageFromClient;
-		while (cs.isConnected()) {
+		String nachrichtVomClient = new String();
+		while (clientSocket.isConnected()) {
 			try {
-<<<<<<< HEAD
-				messageFromClient = cs.getInBR().readLine();
-				broadcastMessage(messageFromClient);
-			} catch (IOException e) {
-				closeEveryThing(cs, cs.getInBR(), cs.getOutBR());
-=======
 				nachrichtVomClient = bufferedReader.readLine();
 				nachrichtAnJedenVersenden(nachrichtVomClient);
 			} catch (IOException e) {
 				allesSchliessen(clientSocket, bufferedReader, bufferedWriter);
->>>>>>> 07b9a8c93f725127e2f4d2dcdd997757fe58bc34
 				break;
 			}
 		}
 	}
 
-	public void broadcastMessage(String messageToSend) {
-		for (ServerThread clientHandler : ServerThreads) {
+	public void nachrichtAnJedenVersenden(String nachricht) {
+		
+		for(int i = 0; i < serverThreads.size(); i++) {
+			ServerThread mServerThread = serverThreads.get(i);
 			try {
-<<<<<<< HEAD
-				if (!clientHandler.clientUserName.equals(clientUserName)) {
-					clientHandler.cs.getOutBR().write(messageToSend);
-					clientHandler.cs.getOutBR().newLine();
-					clientHandler.cs.getOutBR().flush();
-				}
-			} catch (IOException e) {
-				closeEveryThing(cs, cs.getInBR(), cs.getOutBR());
-=======
 				if(!(mServerThread.clientUserName.equals(clientUserName))) {
 					mServerThread.bufferedWriter.write(nachricht);
 					mServerThread.bufferedWriter.newLine();;
@@ -79,17 +53,16 @@ public class ServerThread extends Thread {
 				}
 			} catch (IOException e) {
 				allesSchliessen(clientSocket, bufferedReader, bufferedWriter);
->>>>>>> 07b9a8c93f725127e2f4d2dcdd997757fe58bc34
 			}
 		}
 	}
 
 	public void removeClientHandler() {
-		ServerThreads.remove(this);
-		broadcastMessage("Server: " + clientUserName + "has left the chat");
+		serverThreads.remove(this);
+		nachrichtAnJedenVersenden("Server: " + clientUserName + " hat den Gruppenchat verlassen.");
 	}
 
-	public void closeEveryThing(Socket cs, BufferedReader bufferedReader, BufferedWriter buffredWriter) {
+	public void allesSchliessen(Socket socket, BufferedReader bufferedReader, BufferedWriter buffredWriter) {
 		try {
 			if (bufferedReader != null) {
 				bufferedReader.close();
@@ -97,12 +70,11 @@ public class ServerThread extends Thread {
 			if (bufferedWriter != null) {
 				bufferedWriter.close();
 			}
-			if (cs != null) {
-				cs.close();
+			if (socket != null) {
+				socket.close();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
 }
