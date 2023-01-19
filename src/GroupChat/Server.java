bc4b8be2c692ccd3b134ae210021202b52fa4450
Smen04 <s.mengillioglu@hotmail.com>
2023-01-19 17:17:15 +0100

    New Server

--- src/GroupChat/Server.java
+++ src/GroupChat/Server.java
@@ -5,20 +5,35 @@ import java.io.IOException;
 public class Server {
 
 	private ServerSocket serverSocket;
+	private int port;
+	private boolean isRunning;
 
 	public Server() {
 		serverSocket = null;
+		isRunning = false;
 	}
 
-	public void start() throws IOException {
-		ServerSocket serverSocket = new ServerSocket(8002);
-		Thread t = new Thread(() -> startServer());
+	public void startServer(int port) {
+		this.port = port;
+		isRunning = false;
+		Thread t = new Thread(() -> {
+			try {
+				runServer();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
 		t.start();
 	}
+	
+	public void stopServer() {
+		isRunning = false;
+	}
 
-	public void startServer() {
-
-		while (true) {
+	public void runServer() throws IOException {
+		serverSocket = new ServerSocket(port);
+		while (isRunning) {
 			try {
 				Socket socket = serverSocket.accept();
 				System.out.println("A new client has connected ");
@@ -32,8 +47,7 @@ public class Server {
 	}
 
 	public static void main(String[] args) throws IOException {
-
 		Server server = new Server();
-		server.startServer();
+		server.startServer(8002);
 	}
 }
